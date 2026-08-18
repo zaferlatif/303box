b2545a9f3dd2ccfdb25a960952c703b827a83a00
Zafer Latif <realzozi@gmail.com>
2026-08-18 16:20:54 +0300

    Reset stale runtime caches without touching user sessions

---
+++ cache-reset.20260818-1630.js
@@ -0,0 +1,17 @@
+(() => {
+  'use strict';
+  const EPOCH = '20260818-1630';
+  const KEY = `303box-cache-reset-${EPOCH}`;
+  if (sessionStorage.getItem(KEY)) return;
+  sessionStorage.setItem(KEY, '1');
+
+  // Only runtime/browser caches are cleared. Pattern/session/localStorage data is preserved.
+  if ('caches' in window) {
+    caches.keys().then(keys => Promise.all(keys.map(key => caches.delete(key)))).catch(() => {});
+  }
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.getRegistrations().then(regs => Promise.all(regs.map(reg => reg.unregister()))).catch(() => {});
+  }
+
+  window.__303boxCacheEpoch = EPOCH;
+})();
