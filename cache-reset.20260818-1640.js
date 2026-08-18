8b3937346ef7475062ae01173fa6d86b3feb0e09
Zafer Latif <realzozi@gmail.com>
2026-08-18 16:34:49 +0300

    Bump runtime cache epoch for MIDI safety release

---
+++ cache-reset.20260818-1640.js
@@ -0,0 +1,10 @@
+(() => {
+  'use strict';
+  const EPOCH='20260818-1640';
+  const KEY=`303box-cache-reset-${EPOCH}`;
+  if(sessionStorage.getItem(KEY))return;
+  sessionStorage.setItem(KEY,'1');
+  if('caches' in window){caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))).catch(()=>{});}
+  if('serviceWorker' in navigator){navigator.serviceWorker.getRegistrations().then(regs=>Promise.all(regs.map(r=>r.unregister()))).catch(()=>{});}
+  window.__303boxCacheEpoch=EPOCH;
+})();
