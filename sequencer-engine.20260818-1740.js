018d42541e4a00d1a9a11d8e62b9d4d529b49bb3
Zafer Latif <realzozi@gmail.com>
2026-08-18 21:03:46 +0300

    Apply positioning copy before audio compatibility layers

--- sequencer-engine.20260818-1740.js
+++ sequencer-engine.20260818-1740.js
@@ -11,6 +11,7 @@
   addCss('positioning','./positioning.20260818-1740.css');
   const load=src=>new Promise((resolve,reject)=>{const s=document.createElement('script');s.src=`${src}?v=${version}`;s.async=false;s.onload=resolve;s.onerror=reject;document.head.appendChild(s)});
   load('./cache-reset.20260818-1740.js')
+    .then(()=>load('./seo.20260818-1740.js'))
     .then(()=>load('./transport-fuse.20260818-1530.js'))
     .then(()=>load('./audio-mode-gate.20260818-1530.js'))
     .then(()=>load('./stable-audio-timer.20260818-1600.js'))
@@ -22,7 +23,6 @@
     .then(()=>load('./ui-polish.20260818-1255.js'))
     .then(()=>load('./drum-hat-grid.20260818-1530.js'))
     .then(()=>load('./hero-stability.20260818-1500.js'))
-    .then(()=>load('./seo.20260818-1740.js'))
     .then(()=>load('./generator-router.20260818-1650.js'))
     .catch(error=>console.error('303box production loader',error));
 })();
