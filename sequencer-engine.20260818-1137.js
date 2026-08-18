8d06eaf86cccd248c595998e333cb67957ac9e72
Zafer Latif <realzozi@gmail.com>
2026-08-18 16:05:53 +0300

    Ship independent generators and stable MIDI/scope stack

--- sequencer-engine.20260818-1137.js
+++ sequencer-engine.20260818-1137.js
@@ -1,6 +1,6 @@
 (() => {
   'use strict';
-  const version = '20260818-1542';
+  const version = '20260818-1602';
 
   const addCss = (key, href) => {
     if (document.querySelector(`link[data-303box-${key}]`)) return;
@@ -18,7 +18,7 @@
   addCss('compact-workstation', './compact-workstation.20260818-1430.css');
   addCss('console-io', './console-io.20260818-1440.css');
   addCss('unified-actions', './unified-actions.20260818-1450.css');
-  addCss('midi-router', './midi-router.20260818-1530.css');
+  addCss('midi-router', './midi-router.20260818-1600.css');
   addCss('workstation-layout', './workstation-layout.20260818-1520.css');
 
   const load = src => new Promise((resolve, reject) => {
@@ -32,6 +32,7 @@
 
   load('./transport-fuse.20260818-1530.js')
     .then(() => load('./audio-mode-gate.20260818-1530.js'))
+    .then(() => load('./stable-audio-timer.20260818-1600.js'))
     .then(() => load('./master-reverb.20260818-1510.js'))
     .then(() => load('./drum-level-fix.20260818-1420.js'))
     .then(() => load('./acid-console.20260818-1340.js'))
@@ -40,10 +41,11 @@
     .then(() => load('./ui-polish.20260818-1255.js'))
     .then(() => load('./compact-workstation.20260818-1430.js'))
     .then(() => load('./drum-hat-grid.20260818-1530.js'))
-    .then(() => load('./midi-router.20260818-1530.js'))
-    .then(() => load('./bass-scope.20260818-1500.js'))
+    .then(() => load('./midi-router.20260818-1600.js'))
+    .then(() => load('./bass-scope.20260818-1600.js'))
     .then(() => load('./hero-stability.20260818-1500.js'))
     .then(() => load('./seo.20260818-1315.js'))
     .then(() => load('./workstation-layout.20260818-1520.js'))
+    .then(() => load('./generator-router.20260818-1600.js'))
     .catch(error => console.error('303box production loader', error));
 })();
