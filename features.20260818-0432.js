d03388b09a905885de13035e31ddace68b4073b4
Zafer Latif <realzozi@gmail.com>
2026-08-18 08:57:32 +0300

    Load upper-C note selector patch

--- features.20260818-0432.js
+++ features.20260818-0432.js
@@ -2,5 +2,11 @@
   const script = document.createElement('script');
   script.src = './enhance.20260818-0845.js?v=20260818-0845';
   script.async = false;
+  script.onload = () => {
+    const patch = document.createElement('script');
+    patch.src = './note-top-c.20260818-0845.js?v=20260818-0845';
+    patch.async = false;
+    document.head.appendChild(patch);
+  };
   document.head.appendChild(script);
 })();
