08e3282a374039a2a70c455578661f076f859ee8
Zafer Latif <realzozi@gmail.com>
2026-08-18 18:52:01 +0300

    Bump cache epoch to 1700

---
+++ cache-reset.20260818-1700.js
@@ -0,0 +1,9 @@
+(() => {
+  'use strict';
+  const EPOCH='20260818-1700',KEY=`303box-cache-reset-${EPOCH}`;
+  if(sessionStorage.getItem(KEY))return;
+  sessionStorage.setItem(KEY,'1');
+  if('caches' in window)caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))).catch(()=>{});
+  if('serviceWorker' in navigator)navigator.serviceWorker.getRegistrations().then(rs=>Promise.all(rs.map(r=>r.unregister()))).catch(()=>{});
+  window.__303boxCacheEpoch=EPOCH;
+})();
